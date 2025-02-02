8fc3c503d755830a538b81221d96c2484b4cc10a
Justin <jkimble.dev@gmail.com>
2025-02-01 21:43:18 -0500

    install Intervention, working resize

--- app/Livewire/Metacompress.php
+++ app/Livewire/Metacompress.php
@@ -2,12 +2,62 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Event;
 use Livewire\Component;
+use Illuminate\support\Facades\Storage;
+use Livewire\WithFileUploads;
+use Intervention\Image\ImageManager as Image;
+use Intervention\Image\Drivers\Gd\Driver;
+//use Intervention\Image\Facades\Image as Image;
+
 
 class Metacompress extends Component
 {
+    public $image;
+    use WithFileUploads;
+
     public function render()
     {
         return view('livewire.metacompress');
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'image' => 'required|image',
+        ]);
+
+        // $tempPath = $this->getTempFilePath() . '/original_' . time() . '.' . pathinfo($this->image->getClientOriginalExtension(), PATHINFO_EXTENSION);
+        // Image::make($this->image)->save($tempPath);
+
+        // event(new Event('livewire-upload-init', ['upload' => $this->upload]));
+    }
+
+    // public function downloadCompressedImage()
+    // {
+    //     $compressedPath = $this->getTempFilePath() . '/compressed_' . basename($this->image);
+    //     return response()->download($compressedPath, basename($this->image));
+    // }
+
+    public function compressImage()
+    {
+        $this->validate([
+            'image' => 'required|image',
+        ]);
+
+        if ($this->image) {
+            $path = $this->image->getRealPath();
+            //$image = Image::imagick()->read($path);
+            $image = Image::gd()->read($path);
+            $image->resize(200, 200);
+            //$image = Image::make($path)->encode('jpg', 75); // Adjust the quality here (0-100)
+
+            $compressedPath = storage_path('app/public/compressed_image.jpg');
+            $image->save($compressedPath);
+
+            return response()->download($compressedPath);
+        } else {
+            session()->flash('error', 'No image uploaded.');
+        }
     }
 }
--- resources/views/livewire/metacompress.blade.php
+++ resources/views/livewire/metacompress.blade.php
@@ -1,3 +1,10 @@
 <div>
     <h1>Hello, World!</h1>
+    <div>
+        <form wire:submit.prevent="compressImage">
+            <input type="file" wire:model="image" accept="image/*">
+            <button type="submit">Compress Image</button>
+        </form>
+    </div>
+
 </div>
