ffef26f64f6b001c36acd4eec317f08bb7ea79e4
Justin <jkimble.dev@gmail.com>
2025-02-02 01:22:08 -0500

    before adding daisyui, starting img form

--- app/Livewire/Metacompress.php
+++ app/Livewire/Metacompress.php
@@ -7,25 +7,22 @@ use Livewire\Component;
 use Illuminate\support\Facades\Storage;
 use Livewire\WithFileUploads;
 use Intervention\Image\ImageManager as Image;
+use Intervention\Image\Encoders\AutoEncoder;
+use Intervention\Image\Encoders\WebpEncoder;
+use Intervention\Image\Encoders\PngEncoder;
+use Intervention\Image\Encoders\JpegEncoder;
 use Intervention\Image\Drivers\Gd\Driver;
-//use Intervention\Image\Facades\Image as Image;
 
 
 class Metacompress extends Component
 {
-    public $image;
     use WithFileUploads;
+    public $image;
+    public $size;
 
     public function render()
     {
         return view('livewire.metacompress');
-    }
-
-    public function store()
-    {
-        $this->validate([
-            'image' => 'required|image',
-        ]);
     }
 
     public function compressImage()
@@ -38,10 +35,12 @@ class Metacompress extends Component
             $path = $this->image->getRealPath();
             //$image = Image::imagick()->read($path);
             $image = Image::gd()->read($path);
-            $image->resize(200, 200);
-            //$image = Image::make($path)->encode('jpg', 75); // Adjust the quality here (0-100)
+            $image->toWebp(70);
+            //$image->encode(new WebpEncoder(quality: 65));
+            //$image->resize(200, 200);
+            //$image->scaleDown(400, 300);
 
-            $compressedPath = storage_path('app/public/compressed_image.jpg');
+            $compressedPath = storage_path('app/public/compressed_image.webp');
             $image->save($compressedPath);
 
             return response()->download($compressedPath);
