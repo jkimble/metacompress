74967337d008b3043fb4d89df4b1c65ae66b4b01
Justin <jkimble.dev@gmail.com>
2025-02-02 22:58:37 -0500

    frontend edits

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" async />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
-        {{ $slot }}
+    <body class="bg-white">
+        <div class="container mx-auto">
+            {{ $slot }}
+        </div>
     </body>
 </html>
