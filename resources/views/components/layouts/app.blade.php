1f5cb187e7fef36b23ec4ee94d1f11c2aec4f78a
jkimble <jkimble.dev@gmail.com>
2025-02-23 23:09:07 -0500

    addng route names, link styling not working

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,8 +13,8 @@
             <div class="mx-auto max-w-2xl px-6 lg:px-8 pb-8">
                 {{ $slot }}
                 <nav class="flex flex-row gap-4">
-                    <a class="link link-secondary" wire:navigate href="/" :active="request()->is('/')">Home</a>
-                    <a class="link link-secondary" wire:navigate href="/faqs" :active="request()->is('faqs')">FAQs</a>
+                    <a class="link link-secondary" wire:navigate href="/" :active="request()->routeIs('home')">Home</a>
+                    <a class="link link-secondary" wire:navigate href="/faqs" :active="request()->routeIs('faqs')">FAQs</a>
                 </nav>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Livewire\Faqs;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Metacompress;
 
-Route::get('/', Metacompress::class);
-Route::get('/faqs', Faqs::class);
+Route::get('/', Metacompress::class)->name('home');
+Route::get('/faqs', Faqs::class)->name('faqs');
