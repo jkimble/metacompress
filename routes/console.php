2751c2f9c7ef7a51a1754df7d8218022638f3868
jkimble <jkimble.dev@gmail.com>
2025-03-01 19:07:12 -0500

    add job to clear uploads

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    $directory = storage_path('app/private/livewire-tmp');
+    if (File::exists($directory) && !File::isEmptyDirectory($directory)) {
+        File::cleanDirectory($directory);
+    }
+});
