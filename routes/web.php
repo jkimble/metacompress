42754583b9180798a3031061a2b251e222e66ffb
Justin <jkimble.dev@gmail.com>
2025-08-09 02:58:47 -0400

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Livewire\Metacompress;
 use App\Livewire\Privacy;
 
 Route::get('/', Metacompress::class)->name('home');
-Route::get('/privacy', Privacy::class)->name('home');
+Route::get('/privacy', Privacy::class)->name('privacy');
